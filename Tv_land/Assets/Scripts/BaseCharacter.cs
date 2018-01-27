3788f270b95305b968dbbbe27eae6b4cea4a31ac
Aroosh Bhakhri <aroosh.bhakhri@gmail.com>
2018-01-27 00:35:11 -0500

    Jumping improved

--- Tv_land/Assets/Scripts/BaseCharacter.cs
+++ Tv_land/Assets/Scripts/BaseCharacter.cs
@@ -7,29 +7,24 @@ public class BaseCharacter : MonoBehaviour
     [Header("Speed the character moves with")]
 
     [SerializeField]
-    float maxSpeed;
-    Vector2 input;
+    protected float maxSpeed;
+    protected Vector2 input;
 
     [Header("Distanc player slides on releasing key")]
     [SerializeField]
-    float slideDistance = 5;
+    protected float slideDistance = 5;
 
     [Header("Amount of thrust player gets on jumping")]
     [SerializeField]
-    float jumpUp = 400;
-
-    //[Header("Amount of horizontal thrust player gets on jumping")]
-    //[SerializeField]
-    //float jumpDirection = 10;
+    protected float jumpUp = 400;
 
     //Rigidbody of the component
     protected Rigidbody2D rd;
-
-    //Jump cooldown
-    //[Header("Cooldown for jumping")]
-    //[SerializeField]
-    //protected float jumpCooldown = 1.6f;
-    //protected float jumpVar = 0;
+   
+    void Start()
+    {
+        Initialize();
+    }
 
     // Update is called once per frame
     private void FixedUpdate()
@@ -40,20 +35,17 @@ public class BaseCharacter : MonoBehaviour
     protected void Initialize()
     {
         rd = GetComponent<Rigidbody2D>();
-        //jumpVar = jumpCooldown;
     }
 
     protected void Move()
     {
-        //jumpCooldown -= Time.deltaTime;
-        //Move right
+        
         rd.velocity = new Vector2(input.x * maxSpeed *  1, rd.velocity.y);
 
         //Jump
-        if (Input.GetKey(KeyCode.Space) && Physics.Raycast(transform.position, Vector3.down, 1))
+        if (Input.GetKey(KeyCode.Space) && Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - transform.localScale.y / 2 - 0.2f), Vector2.down, 0.1f))
         {
             rd.AddForce(new Vector3(0, jumpUp, 0));
-           // jumpCooldown = jumpVar;
         }
         //Todo: play crouch animation
         if(Input.GetKey(KeyCode.C))
