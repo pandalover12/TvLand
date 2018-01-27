08d8f417f2f9b98f08e6328ba40162c5cf554bee
Arthur Parker <aeparker101@mail.com>
2018-01-27 06:19:53 -0500

    RealmControl.cs

---
+++ Tv_land/Assets/Scripts/Materalcontrol.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Materalcontrol : MonoBehaviour {
+    [SerializeField]
+    GameObject[] ALWAYSTHESAMEbutnotsamemat = new GameObject[100];
+    GameObject[] extraherostuff = new GameObject[10];
+    GameObject[] extrawesternstuff = new GameObject[10];
+    GameObject[] extrahamer = new GameObject[10];
+    public  int switchmat = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (switchmat == 0) {
+
+            for (int i = 0; i < 10; ++i)
+            {
+                ALWAYSTHESAMEbutnotsamemat[i].GetComponent<my3mats>().swtcher = 0;
+            }
+           for(int i =0; i < extraherostuff.Length; ++i)
+            {
+                extraherostuff[i].SetActive(  true);
+            }
+            for (int i = 0; i < extrawesternstuff.Length; ++i)
+            {
+                extrawesternstuff[i].SetActive(false);
+            }
+            for (int i = 0; i < extrahamer.Length; ++i)
+            {
+                extrahamer[i].SetActive(false);
+            }
+
+        }
+   else     if (switchmat == 1)
+        {
+
+            for (int i = 0; i < 10; ++i)
+            {
+                ALWAYSTHESAMEbutnotsamemat[i].GetComponent<my3mats>().swtcher = 1;
+            }
+            for (int i = 0; i < extraherostuff.Length; ++i)
+            {
+                extraherostuff[i].SetActive(false);
+            }
+            for (int i = 0; i < extrawesternstuff.Length; ++i)
+            {
+                extrawesternstuff[i].SetActive(true);
+            }
+            for (int i = 0; i < extrahamer.Length; ++i)
+            {
+                extrahamer[i].SetActive(false);
+            }
+
+        }
+ else       if (switchmat == 2)
+        {
+
+            for (int i = 0; i < 10; ++i)
+            {
+                ALWAYSTHESAMEbutnotsamemat[i].GetComponent<my3mats>().swtcher = 2;
+            }
+            for (int i = 0; i < extraherostuff.Length; ++i)
+            {
+                extraherostuff[i].SetActive(false);
+            }
+            for (int i = 0; i < extrawesternstuff.Length; ++i)
+            {
+                extrawesternstuff[i].SetActive(false);
+            }
+            for (int i = 0; i < extrahamer.Length; ++i)
+            {
+                extrahamer[i].SetActive(true);
+            }
+
+        }
+    }
+}
---
+++ Tv_land/Assets/Scripts/RealmControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealmControl : MonoBehaviour {
+    [SerializeField]
+    WesternHero westhero;
+    [SerializeField]
+    MidievalHero hamerguy;
+    [SerializeField]
+    SuperHero superguys;
+    [SerializeField]
+    Materalcontrol mat;
+
+	// Use this for initialization
+	void Start () {
+        superguys.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((Input.GetKey(KeyCode.L))) { 
+            if (superguys.enabled == true)
+            {
+                mat.switchmat = 1;
+                superguys.enabled = false;
+                westhero.enabled = true;
+            }
+            else if (westhero.enabled == true)
+            {
+                mat.switchmat = 2;
+                westhero.enabled = false;
+                hamerguy.enabled = true;
+            }
+
+            else
+            {
+                mat.switchmat = 0;
+                hamerguy.enabled = false;
+                superguys.enabled = true;
+            }
+        }
+
+    }
+}
