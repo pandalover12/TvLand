a62dc0c42442fb39ae7f997713b171931ec6fb49
Arthur Parker <aeparker101@mail.com>
2018-01-27 06:46:37 -0500

    doen the RealmControl

--- Tv_land/Assets/Scripts/RealmControl.cs
+++ Tv_land/Assets/Scripts/RealmControl.cs
@@ -11,6 +11,7 @@ public class RealmControl : MonoBehaviour {
     SuperHero superguys;
     [SerializeField]
     Materalcontrol mat;
+    bool button = false;
 
 	// Use this for initialization
 	void Start () {
@@ -19,15 +20,17 @@ public class RealmControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if ((Input.GetKey(KeyCode.L))) { 
+        if ((Input.GetKey(KeyCode.L))&& button==false) { 
             if (superguys.enabled == true)
             {
+              
                 mat.switchmat = 1;
                 superguys.enabled = false;
                 westhero.enabled = true;
             }
             else if (westhero.enabled == true)
             {
+               
                 mat.switchmat = 2;
                 westhero.enabled = false;
                 hamerguy.enabled = true;
@@ -35,11 +38,19 @@ public class RealmControl : MonoBehaviour {
 
             else
             {
+               
                 mat.switchmat = 0;
                 hamerguy.enabled = false;
                 superguys.enabled = true;
             }
+            button = true;
+            StartCoroutine(buttonbounce());
         }
 
+    }
+    IEnumerator buttonbounce()
+    {
+        yield return new WaitForSeconds(0.4f);
+        button = false;
     }
 }
