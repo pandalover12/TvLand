2bb5156e3d2b7eb4fa726fa8d56f8ab702da9613
Aroosh Bhakhri <aroosh.bhakhri@gmail.com>
2018-01-27 18:52:03 -0500

    Commit for pull

--- Tv_land/Assets/Scripts/BreakableObject.cs
+++ Tv_land/Assets/Scripts/BreakableObject.cs
@@ -5,15 +5,36 @@ using UnityEngine;
 public class BreakableObject : MonoBehaviour
 {
 
+    [SerializeField]
+    GameObject destructionSphere;
+
+    [SerializeField]
+    int numParticles = 10;
+    [SerializeField]
+    float explosionForce = 10;
+    [SerializeField]
+    float explosionRange = 10;
+
     private void OnTriggerStay(Collider collision)
     {
     
         if(collision.gameObject.tag == "Hammer" && Input.GetKey(KeyCode.Mouse0))
         {
-            GetComponent<Rigidbody>().AddForce(new Vector2(0, 10));
-            GetComponent<Rigidbody>().AddForce(new Vector2(1000, 10));
 
+            Debug.Log("Blast");
 
+            for (int i = 0; i < numParticles; i++)
+            {
+                float randomX = UnityEngine.Random.Range(0, transform.localScale.x / 2);
+                float randomY = UnityEngine.Random.Range(0, transform.localScale.y / 2);
+                float randomZ = UnityEngine.Random.Range(0, transform.localScale.z / 2);
+
+                GameObject tempVar = Instantiate(destructionSphere, new Vector3(randomX, randomY, randomZ), Random.rotation);
+                tempVar.GetComponent<Rigidbody>().AddExplosionForce(explosionForce, transform.position, explosionRange);
+                Destroy(tempVar, 2f);
+            }
+
+            Destroy(gameObject);
 
           //  GetComponent<Rigidbody>().AddExplosionForce(100, collision.transform.position, 1000);
         }
