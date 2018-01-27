231f0a3bcab6d3111d50295a5010b04fac337d8e
Arthur Parker <aeparker101@mail.com>
2018-01-27 04:23:57 -0500

    grapple almostgood

--- Tv_land/Assets/Scripts/GrappleHook.cs
+++ Tv_land/Assets/Scripts/GrappleHook.cs
@@ -187,7 +187,7 @@ public class GrappleHook : MonoBehaviour {
         {
             reelingIn = true;
         float d=    Vector2.Distance(eye.transform.position, transform.position);
-            Debug.Break();
+         //   Debug.Break();
         }
         if (collision == false && (Vector2)transform.position == destiny)
         {
