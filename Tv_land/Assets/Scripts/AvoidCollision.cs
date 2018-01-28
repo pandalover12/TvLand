aa63610f9098f7a487d6ccca341dc7cd5b68f04e
Aroosh Bhakhri <aroosh.bhakhri@gmail.com>
2018-01-27 20:17:09 -0500

    Destroy-able sphere done

---
+++ Tv_land/Assets/Scripts/AvoidCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvoidCollision : MonoBehaviour
+{
+
+    GameObject[] destructionParticles;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        destructionParticles = new GameObject[GameObject.FindGameObjectsWithTag("DestructionParticle").Length];
+        destructionParticles = GameObject.FindGameObjectsWithTag("DestructionParticle");
+
+        for (int i = 0; i < destructionParticles.Length; i++)
+        {
+            Physics.IgnoreCollision(GetComponent<Collider>(), destructionParticles[i].GetComponent<Collider>());
+        }
+
+    }
+}
